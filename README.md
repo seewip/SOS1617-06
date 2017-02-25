d3fca580138c2e62a5d49d2fdbcfaf4755adf37d
sos161706cle <crileaech@gmail.com>
2017-02-25 11:59:35 +0000

    Launch current date and time

---
+++ README.md
@@ -0,0 +1,22 @@
+# SOS1617-06
+#Alumnos:
+Mateusz Dominik
+
+Jihane Fahri
+
+Cristina Leal Echevarría
+         
+Nuestro trabajo consistirá en reunir los datos del área de educación que se encuentren abiertos
+Los países elegidos de donde hemos obtenido estos datos son España, Polonia y Marruecos
+
+#Repositorio
+
+SOS1617/SOS1617-06
+
+#Canal slack
+
+https://sos1617.slack.com/messages/sos1617-06/
+
+
+         
+    
---
+++ index.js
@@ -0,0 +1,44 @@
+var express = require("express");
+var app = express();
+var port = process.env.PORT  ;
+
+//app.use("/", express.static(__dirname + "static"));
+
+app.listen(port, (err) => {
+    if (!err)
+        console.log("Web Server is running and listening on port: " + port);
+    else
+        console.log("ERROR initializing server on port " + port + ":" + err);
+});
+
+
+app.get("/", (req, res)=>{
+    res.send("<html><body><h1> Hello SOS1617 from the group G06</h1></body></html>");
+    
+});
+
+app.get("/time", (req, res)=>{
+    res.send(getFecha());
+});
+
+console.log("La fecha actual es :" +getFecha());
+
+
+function getFecha(){
+    var mes = new Array("January","February", "March", "April", "May", "June", 
+    "July", "August", "September", "November", "October", "DEcember");
+    
+    var diasSemana = new Array("Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+    
+    var fecha = new Date();
+    var m = fecha.getMinutes();
+    var s = fecha.getSeconds();
+    
+    var minutos  = (m < 10) ? '0' + m : m;
+    var segundos = (s < 10) ? '0' +s : s; 
+    
+    var ret = diasSemana[fecha.getDay()] + "," + fecha.getDate() +  "of"  + mes[fecha.getMonth()]
+            + "of" +fecha.getFullYear() + "," +fecha.getHours() + ":" +minutos + ":" +segundos + ",";
+    return ret;
+}
+
