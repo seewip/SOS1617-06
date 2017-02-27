2d5f9a85a390634a5357da7aa534131cd16dab2a
SOS161707md <mateuszjacek95@gmail.com>
2017-02-27 12:53:33 +0000

    Fixes #9

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var express = require("express");
-var app = express();
-var port = process.env.PORT  ;
 
-//app.use("/", express.static(__dirname + "static"));
+var app = express();
+var port = process.env.PORT;
 
 app.listen(port, (err) => {
     if (!err)
@@ -12,35 +11,29 @@ app.listen(port, (err) => {
 });
 
 
-app.get("/", (req, res)=>{
-    res.send("<html><body><h1> Hello SOS1617 from the group G06</h1></body></html>");
-    
+app.get("/", (req, res) => {
+    res.send('<html><body><h1>Hello SOS1617 from the group G06</h1></br><a href="time">Get current time</a></body></html>');
+
 });
 
-app.get("/time", (req, res)=>{
-    res.send(getFecha());
+app.get("/time", (req, res) => {
+    res.send("<html><body><p>Current time is: "+getDate()+"</p></body></html>");
 });
 
-console.log("La fecha actual es :" +getFecha());
+console.log("The current date is: " + getDate());
 
+function getDate() {
+    var month = new Array("January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "November", "October", "December");
 
-function getFecha(){
-    var mes = new Array("January","February", "March", "April", "May", "June", 
-    "July", "August", "September", "November", "October", "DEcember");
-    
-    // var diasSemana = new Array("Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
-    //+diasSemana[fecha.getDay()]+
-    
-    var fecha = new Date();
-    
-    var m = fecha.getMinutes();
-    var s = fecha.getSeconds();
-    
-    var minutos  = (m < 10) ? '0' + m : m;
-    var segundos = (s < 10) ? '0' +s : s; 
-    
-    var ret = fecha.getDate()+ " " +mes[fecha.getMonth()]
-            + " of " +fecha.getFullYear()+ " , " +fecha.getHours()+ ":" +minutos+ ":" +segundos;
-    return ret;
+    var date = new Date();
+
+    var minute = date.getMinutes();
+    var second = date.getSeconds();
+
+    minute = (minute < 10) ? '0' + minute : minute;
+    second = (second < 10) ? '0' + second : second;
+
+    return date.getDate() + " " + month[date.getMonth()] +
+        " of " + date.getFullYear() + " , " + date.getHours() + ":" + minute + ":" + second;
 }
-
