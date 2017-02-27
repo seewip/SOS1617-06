0c853598705d12efbd9daa56c7a6fa7577e0749e
sos161706cle <crileaech@gmail.com>
2017-02-27 13:10:54 +0000

    Modify style ident and spaces

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ app.listen(port, (err) => {
 
 
 app.get("/", (req, res) => {
-    res.send('<html><body><h1>Hello SOS1617 from the group G06</h1></br><a href="time">Get current time</a></body></html>');
+    res.send('<html><body><h1>Hello SOS1617 from the group G06</h1></br><a href="time"> Get current time </a></body></html>');
 
 });
 
 app.get("/time", (req, res) => {
-    res.send("<html><body><p>Current time is: " +getDate()+ "</p></body></html> ");
+    res.send("<html><body><p> The Current time is : " +getDate()+ "</p></body></html> ");
 });
 
 console.log("The current date is: " + getDate());
