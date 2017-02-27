b1d182f25a6b35f6bff842422333ac76bde96a58
sos161706cle <crileaech@gmail.com>
2017-02-27 12:44:36 +0000

    Changed code in index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var express = require("express");
 var app = express();
 var port = process.env.PORT  ;
 
-//app.use("/", express.static(__dirname + "static"));
 
 app.listen(port, (err) => {
     if (!err)
