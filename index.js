05f9e347f5a6fddb8e5826c990a127a7e308f6b6
SOS161707md <mateuszjacek95@gmail.com>
2017-03-28 11:32:11 +0000

    Added tests

--- index.js
+++ index.js
@@ -50,6 +50,15 @@ MongoClient.connect(mdbURL,{native_parser:true}, function(err,database){
 app.use("/",express.static(publicFolder));
 //=================================BOTTON FOR RUN POSTMAN====================================================//
 
+app.get(BASE_API_PATH + "/tests", function(request, response) {
+    response.send("<html><head><title>Tests</title></head><body><h1>Tests</h1>"
+    +"<p>Cristina Leal Echevarría - gdp: </p>"
+    +'<p>Mateusz Dominik - education: </p><div class="postman-run-button" data-postman-action="collection/import" data-postman-var-1="f2250ecc94e7e9fb7375"></div>'
+    +"<p>Jihane Fahri - gdp-per-capita: </p>"
+    // Script to generate button
+    +'<script type="text/javascript">(function (p,o,s,t,m,a,n) {!p[s] && (p[s] = function () { (p[t] || (p[t] = [])).push(arguments); });!o.getElementById(s+t) && o.getElementsByTagName("head")[0].appendChild(((n = o.createElement("script")),(n.id = s+t), (n.async = 1), (n.src = m), n));}(window, document, "_pm", "PostmanRunObject", "https://run.pstmn.io/button.js"));</script>'
+    +"</body></html>");
+});
 
 //====================================CODIGO API Jihane==================================================================//
 /*========================================Load Initial Data===============================================================*/
