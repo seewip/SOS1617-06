fdb13513e055f62411a1bb8cba1a624cb8e017d7
sos161706jf <jihfahri@gmail.com>
2017-04-25 19:05:41 +0000

    f08

--- public/gdp-per-capita/gdp-per-capita-edit-ctrl.js
+++ public/gdp-per-capita/gdp-per-capita-edit-ctrl.js
@@ -5,16 +5,31 @@ var nextPage;
 var setPage;
 
 angular.module("DataManagementApp").
-controller("GdpPerCapitaEditCtrl", ["$scope", "$http", "$routeParams", "$location", function($scope, $http, $routeParams, $location) {
+controller("GdpPerCapitaEditCtrl", ["$scope", "$http", "$routeParams", "$location", "$rootScope", function($scope, $http, $routeParams, $location, $rootScope) {
     console.log("Controller initialized (GdpPerCapitaEditCtrl)");
     
-    var apikey = "secret";
+    if (!$rootScope.apikey) $rootScope.apikey = "secret";
 
     function refresh() {
         $http
-            .get("../api/v1/gdp-per-capita/" + $routeParams.country + "/" + $routeParams.year + "?" + "apikey=" + apikey)
+            .get("../api/v1/gdp-per-capita/" + $routeParams.country + "/" + $routeParams.year + "?" + "apikey=" + $rootScope.apikey)
             .then(function(response) {
-                $scope.editDataUnit = response.data;
+                $scope.editDataUnit = response.data[0];
+            }, function(response) {
+                switch (response.status) {
+                    case 401:
+                        Materialize.toast('<i class="material-icons">error_outline</i> Error getting data - api key missing!', 4000);
+                        break;
+                    case 403:
+                        Materialize.toast('<i class="material-icons">error_outline</i> Error getting data - api key incorrect!', 4000);
+                        break;
+                    case 404:
+                        Materialize.toast('<i class="material-icons">error_outline</i> Error getting data - data not found!', 4000);
+                        break;
+                    default:
+                        Materialize.toast('<i class="material-icons">error_outline</i> Error getting data!', 4000);
+                        break;
+                }
             });
     }
     
@@ -23,39 +38,29 @@ controller("GdpPerCapitaEditCtrl", ["$scope", "$http", "$routeParams", "$locatio
         $location.path('/gdp-per-capita');
     };
     
-    $scope.editData = function() {
-        console.log("Unimplemented!");
-    };
-
-    /* $scope.editData = function(data) {
-
-        var oldCountry = data.oldCountry;
-        var oldYear = data.oldYear;
-        delete data._id;
-        delete data.oldCountry;
-        delete data.oldYear;
-
-        data.year = Number(data.year);
+     $scope.editData = function(data) {
         $http
-            .put("../api/v1/gdp-per-capita/" + oldCountry + "/" + oldYear + "?" + "apikey=" + $scope.apikey, data)
+            .put("../api/v1/gdp-per-capita/" + data.country + "/" + data.year + "?" + "apikey=" + $rootScope.apikey, data)
             .then(function(response) {
                 console.log("Data " + data.country + " edited!");
-                Materialize.toast('<i class="material-icons">done</i> ' + oldCountry + ' has been edited succesfully!', 4000);
-                refresh();
+                Materialize.toast('<i class="material-icons">done</i> ' + data.country + ' has been edited succesfully!', 4000);
+                $location.path('/gdp-per-capita');
             }, function(response) {
-                Materialize.toast('<i class="material-icons">error_outline</i> Error editing data!', 4000);
-                refresh();
+                switch (response.status) {
+                    case 400:
+                        Materialize.toast('<i class="material-icons">error_outline</i> Error editing data - incorrect data was typed!', 4000);
+                        break;
+                    case 401:
+                        Materialize.toast('<i class="material-icons">error_outline</i> Error getting data - api key missing!', 4000);
+                        break;
+                    case 403:
+                        Materialize.toast('<i class="material-icons">error_outline</i> Error getting data - api key incorrect!', 4000);
+                        break;
+                    default:
+                        Materialize.toast('<i class="material-icons">error_outline</i> Error editing data!', 4000);
+                        break;
+                }
             });
     };
-    
-    $scope.updateContact = function(contact) {
-         $http
-             .put("api/v1/contacts/" + $routeParams.name, contact)
-             .then(function(response) {
-                 console.log("Contact " + contact.name + " edited!");
-                 $location.path("/");
-             });
-    };*/
-
     refresh();
 }]);
--- public/gdp-per-capita/gdp-per-capita-list-ctrl.js
+++ public/gdp-per-capita/gdp-per-capita-list-ctrl.js
@@ -1,28 +1,77 @@
 /* global angular */
 /* global Materialize */
+/* global $ */
 
 var previousPage;
 var nextPage;
 var setPage;
 
 angular.module("DataManagementApp").
-controller("GdpPerCapitaListCtrl", ["$scope", "$http", function($scope, $http) {
+controller("GdpPerCapitaListCtrl", ["$scope", "$http", "$rootScope", function($scope, $http, $rootScope) {
     console.log("Controller initialized (GdpPerCapitaListCtrl)");
 
-    $scope.apikey = "secret";
+    if (!$rootScope.apikey) $rootScope.apikey = "secret";
+    
     $scope.search = {};
     $scope.searchAdd = {};
 
+    $scope.data = {};
     var dataCache = {};
     $scope.currentPage = 1;
     $scope.maxPages = 1;
     $scope.pages = [];
-
-    var elementsPerPage = 5;
+    $scope.pagesLeft = [];
+    $scope.pagesMid = [];
+    $scope.pagesRight = [];
+    
+    var modifier = "";
+    var properties = "";
+    
+    var elementsPerPage = 3;
 
     function setPagination() {
-        $scope.pages = [];
-        for (var i = 1; i <= $scope.maxPages; i++) $scope.pages.push(i);
+        var pagesNearby = 3;
+        $scope.pagesLeft = [];
+        $scope.pagesMid = [];
+        $scope.pagesRight = [];
+        if ($scope.maxPages <= pagesNearby * 2) {
+            for (var i = 1; i <= $scope.maxPages; i++) $scope.pagesLeft.push(i);
+        }
+        else if ($scope.currentPage >= 0 && $scope.currentPage <= pagesNearby) {
+            //console.log("Left");
+            //only left and mid
+            for (var i = 1; i <= pagesNearby; i++) $scope.pagesLeft.push(i);
+            for (i = $scope.maxPages - pagesNearby + 1; i <= $scope.maxPages; i++) $scope.pagesMid.push(i);
+        }
+        else if ($scope.currentPage >= $scope.maxPages - pagesNearby + 1 && $scope.currentPage <= $scope.maxPages) {
+            //console.log("Right");
+            //only left and mid
+            for (var i = 1; i <= pagesNearby; i++) $scope.pagesMid.push(i);
+            for (i = $scope.maxPages - pagesNearby + 1; i <= $scope.maxPages; i++) $scope.pagesRight.push(i);
+        }
+        else {
+            //console.log("Mid");
+            for (var i = 1; i <= pagesNearby; i++) $scope.pagesLeft.push(i);
+            for (var i = Math.max($scope.currentPage - 1, pagesNearby + 1); i <= Math.min($scope.currentPage + 1, $scope.maxPages - pagesNearby); i++) $scope.pagesMid.push(i);
+            for (i = $scope.maxPages - pagesNearby + 1; i <= $scope.maxPages; i++) $scope.pagesRight.push(i);
+            if (($scope.pagesLeft[$scope.pagesLeft.length - 1] == $scope.pagesMid[0] - 1) && ($scope.pagesMid[$scope.pagesMid.length - 1] == $scope.pagesRight[0] - 1)) {
+                //console.log("JOIN BOTH");
+                $scope.pagesMid = $scope.pagesMid.concat($scope.pagesRight);
+                $scope.pagesLeft = $scope.pagesLeft.concat($scope.pagesMid);
+                $scope.pagesMid = [];
+                $scope.pagesRight = [];
+            }
+            else if ($scope.pagesLeft[$scope.pagesLeft.length - 1] == $scope.pagesMid[0] - 1) {
+                //console.log("JOIN MID INTO LEFT");
+                $scope.pagesLeft = $scope.pagesLeft.concat($scope.pagesMid);
+                $scope.pagesMid = [];
+            }
+            else if ($scope.pagesMid[$scope.pagesMid.length - 1] == $scope.pagesRight[0] - 1) {
+                //console.log("JOIN MID INTO RIGHT");
+                $scope.pagesRight = $scope.pagesMid.concat($scope.pagesRight);
+                $scope.pagesMid = [];
+            }
+        }
     }
 
     $scope.setPage = function(page) {
@@ -44,44 +93,47 @@ controller("GdpPerCapitaListCtrl", ["$scope", "$http", function($scope, $http) {
         if ($scope.currentPage <= 0) $scope.currentPage = 1;
         if ($scope.currentPage > $scope.maxPages) $scope.currentPage = $scope.maxPages;
         setPagination();
-        $scope.data = dataCache.slice(Number(($scope.currentPage - 1) * elementsPerPage), Number(($scope.currentPage) * elementsPerPage));
+        if (dataCache.length > elementsPerPage) {
+            $scope.data = dataCache.slice(Number(($scope.currentPage - 1) * elementsPerPage), Number(($scope.currentPage) * elementsPerPage));
+        }
+        else {
+            $scope.data = dataCache;
+        }
     };
 
     var refresh = $scope.refresh = function() {
-
-        var modifier = "";
-        var properties = "";
-        if ($scope.search.country && $scope.search.year) {
-            modifier = "/" + $scope.search.country + "/" + $scope.search.year;
-        }
-        else if ($scope.search.country) {
-            modifier = "/" + $scope.search.country;
-        }
-        else if ($scope.search.year) {
-            modifier = "/" + $scope.search.year;
-        }
-        for (var prop in $scope.searchAdd) {
-            if ($scope.searchAdd.hasOwnProperty(prop) && prop) {
-                properties += prop + "=" + $scope.searchAdd[prop] + "&";
-            }
-        }
-
-        $http
-            .get("../api/v1/gdp-per-capita" + modifier + "?" + "apikey=" + $scope.apikey + "&" + properties)
+         $http
+            .get("../api/v1/gdp-per-capita" + modifier + "?" + "apikey=" + $rootScope.apikey + "&" + properties)
             .then(function(response) {
-                //console.log("GET: " + "../api/v1/gdp-per-capita" + modifier + "?" + "apikey=" + $scope.apikey + "&" + properties);
                 $scope.maxPages = Math.max(Math.ceil(response.data.length / elementsPerPage), 1);
                 dataCache = response.data;
+                //console.log(JSON.stringify(dataCache, null, 2));
                 $scope.refreshPage();
             }, function(response) {
-                Materialize.toast('<i class="material-icons">error_outline</i> Error getting data!', 4000);
+                switch (response.status) {
+                    case 401:
+                        Materialize.toast('<i class="material-icons">error_outline</i> Error getting data - api key missing!', 4000);
+                        break;
+                    case 403:
+                        Materialize.toast('<i class="material-icons">error_outline</i> Error getting data - api key incorrect!', 4000);
+                        break;
+                    case 404:
+                        $scope.maxPages = 1;
+                        dataCache = {};
+                        $scope.refreshPage();
+                        Materialize.toast('<i class="material-icons">error_outline</i> No data found!', 4000);
+                        break;
+                    default:
+                        Materialize.toast('<i class="material-icons">error_outline</i> Error getting data!', 4000);
+                        break;
+                }
             });
     }
 
 
     $scope.addData = function() {
         $http
-            .post("../api/v1/gdp-per-capita" + "?" + "apikey=" + $scope.apikey, $scope.newData)
+            .post("../api/v1/gdp-per-capita" + "?" + "apikey=" + $rootScope.apikey, $scope.newData)
             .then(function(response) {
                 console.log("Data added!");
                 Materialize.toast('<i class="material-icons">done</i> ' + $scope.newData.country + ' has been added succesfully!', 4000);
@@ -91,37 +143,9 @@ controller("GdpPerCapitaListCtrl", ["$scope", "$http", function($scope, $http) {
             });
     };
 
-    /*$scope.editDataModal = function(data) {
-        data["oldCountry"] = data["country"];
-        data["oldYear"] = data["year"];
-        $scope.editDataUnit = data;
-        $('#editModal').modal('open');
-    };
-
-    $scope.editData = function(data) {
-
-        var oldCountry = data.oldCountry;
-        var oldYear = data.oldYear;
-        delete data._id;
-        delete data.oldCountry;
-        delete data.oldYear;
-
-        data.year = Number(data.year);
-        $http
-            .put("../api/v1/gdp-per-capita/" + oldCountry + "/" + oldYear + "?" + "apikey=" + $scope.apikey, data)
-            .then(function(response) {
-                console.log("Data " + data.country + " edited!");
-                Materialize.toast('<i class="material-icons">done</i> ' + oldCountry + ' has been edited succesfully!', 4000);
-                refresh();
-            }, function(response) {
-                Materialize.toast('<i class="material-icons">error_outline</i> Error editing data!', 4000);
-                refresh();
-            });
-    };*/
-
     $scope.delData = function(data) {
         $http
-            .delete("../api/v1/gdp-per-capita/" + data.country + "/" + data.year + "?" + "apikey=" + $scope.apikey)
+            .delete("../api/v1/gdp-per-capita/" + data.country + "/" + data.year + "?" + "apikey=" + $rootScope.apikey)
             .then(function(response) {
                 console.log("Data " + data.country + " deleted!");
                 Materialize.toast('<i class="material-icons">done</i> ' + data.country + ' has been deleted succesfully!', 4000);
@@ -133,7 +157,7 @@ controller("GdpPerCapitaListCtrl", ["$scope", "$http", function($scope, $http) {
 
     $scope.delAllData = function() {
         $http
-            .delete("../api/v1/gdp-per-capita" + "?" + "apikey=" + $scope.apikey)
+            .delete("../api/v1/gdp-per-capita" + "?" + "apikey=" + $rootScope.apikey)
             .then(function(response) {
                 console.log("All data deleted!");
                 Materialize.toast('<i class="material-icons">done</i> All data has been deleted succesfully!', 4000);
@@ -147,7 +171,7 @@ controller("GdpPerCapitaListCtrl", ["$scope", "$http", function($scope, $http) {
         refresh();
         if ($scope.data.length == 0) {
             $http
-                .get("../api/v1/gdp-per-capita/loadInitialData" + "?" + "apikey=" + $scope.apikey)
+                .get("../api/v1/gdp-per-capita/loadInitialData" + "?" + "apikey=" + $rootScope.apikey)
                 .then(function(response) {
                     console.log("Initial data loaded");
                     Materialize.toast('<i class="material-icons">done</i> Loaded inital data succesfully!', 4000);
@@ -164,16 +188,58 @@ controller("GdpPerCapitaListCtrl", ["$scope", "$http", function($scope, $http) {
 
     refresh();
 
-   /* $(document).ready(function() {
-        $('.modal').modal({
-            ready: function(modal, trigger) {
-                Materialize.updateTextFields();
-            },
-            complete: function() {
-                refresh();
+    $('#apikeyModal').modal({
+        complete: function() {
+            $rootScope.apikey = $scope.apikey;
+
+            $http
+                .get("../api/v1/gdp-per-capita" + modifier + "?" + "apikey=" + $rootScope.apikey + "&" + properties)
+                .then(function(response) {
+                    Materialize.toast('<i class="material-icons">done</i> Api key changed successfully!', 4000);
+                    $scope.maxPages = Math.max(Math.ceil(response.data.length / elementsPerPage), 1);
+                    dataCache = response.data;
+                    $scope.refreshPage();
+                }, function(response) {
+                    $scope.maxPages = 1;
+                    dataCache = {};
+                    $scope.refreshPage();
+                    switch (response.status) {
+                        case 401:
+                            Materialize.toast('<i class="material-icons">error_outline</i> Error getting data - api key missing!', 4000);
+                            break;
+                        case 403:
+                            Materialize.toast('<i class="material-icons">error_outline</i> Error getting data - api key incorrect!', 4000);
+                            break;
+                        default:
+                            Materialize.toast('<i class="material-icons">error_outline</i> Error getting data!', 4000);
+                            break;
+                    }
+                });
+            console.log("Api key changed!");
+        }
+    });
+
+    $('#searchModal').modal({
+        complete: function() {
+            modifier = "";
+            properties = "";
+            if ($scope.search.country && $scope.search.year) {
+                modifier = "/" + $scope.search.country + "/" + $scope.search.year;
             }
-        });
-        $(".button-collapse").sideNav();
-        $(".dropdown-button").dropdown();
-    });*/
+            else if ($scope.search.country) {
+                modifier = "/" + $scope.search.country;
+            }
+            else if ($scope.search.year) {
+                modifier = "/" + $scope.search.year;
+            }
+            for (var prop in $scope.searchAdd) {
+                if ($scope.searchAdd.hasOwnProperty(prop) && prop) {
+                    properties += prop + "=" + $scope.searchAdd[prop] + "&";
+                }
+            }
+
+            refresh();
+        }
+    });
+ 
 }]);
