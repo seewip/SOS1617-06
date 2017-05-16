2de4484eb50053c1b3fc5895b6d083f6ac526345
sos161706cle <crileaech@gmail.com>
2017-05-16 21:49:05 +0000

    Finally everthing work include remote

--- public/gdp/gdp-proxy-graph-ctrl.js
+++ public/gdp/gdp-proxy-graph-ctrl.js
@@ -1,5 +1,3 @@
-// 
-
 
 /*global angular*/
 /*global Highcharts*/
@@ -7,10 +5,10 @@
 
 angular
     .module("DataManagementApp")
-    .controller("GdpProxyGraphCtrl",["$scope","$http",function ($scope, $http){
-        
+    .controller("GdpProxyGraphCtrl", ["$scope", "$http", function($scope, $http) {
+
         console.log("GdpProxyGraphCtrl");
-        
+
         $scope.apikey = "secret";
         $scope.data = {};
         var dataCache = {};
@@ -23,96 +21,104 @@ angular
         $scope.priceaceite = [];
         $scope.pricevirgen = [];
         $scope.priceextra = [];
-        
-        
-        
+
+
+
         function capitalizeFirstLetter(string) {
-                return string.charAt(0).toUpperCase() + string.slice(1);
+            return string.charAt(0).toUpperCase() + string.slice(1);
         }
-        
-        
-            
+
+
+
         $http
             .get("https://sos1617-04.herokuapp.com/api/v2/price-stats?apikey=12345")
             .then(function(response) {
                 dataCache = response.data;
                 $scope.data = dataCache;
-            
-            for(var i=0; i<response.data.length; i++){
-                $scope.province.push(capitalizeFirstLetter($scope.data[i].province) + " " + $scope.data[i].year);
-                $scope.year.push(($scope.data[i].year));
-                $scope.priceaceite.push(Number($scope.data[i].priceaceite));
-                $scope.pricevirgen.push(Number($scope.data[i].pricevirgen));
-                $scope.priceextra.push(Number($scope.data[i].priceextra));
-                
-                console.log($scope.data[i].province);
-        
-            
-                    }  
-                });
-        
-        $http.get("/api/v1/gdp/"+ "?" + "apikey=" + $scope.apikey).then(function(response){
-            
-            dataCache = response.data;
-            $scope.data = dataCache;
-            
-            for(var i=0; i<response.data.length; i++){
-                $scope.country.push(capitalizeFirstLetter($scope.data[i].country) + " " + $scope.data[i].year);
-                //$scope.year.push(Number($scope.data[i].year));
-                $scope.gdp.push(Number($scope.data[i].gdp));
-                $scope.gdp_growth.push(Number($scope.data[i].gdp_growth));
-                $scope.gdp_deflator.push(Number($scope.data[i].gdp_deflator));
-                
-                console.log($scope.data[i].country);
-            }
-            Highcharts.chart('container',{
-                title: {
-                    text: 'Highcharts'
-                },
-                chart: {
-                    type: 'area'
-                },
-                xAxis: {
-                    categories: $scope.country
-                },
-                legend: {
-                    layout: 'vertical',
-                    floating: true,
-                    backgroundColor: '#FFFFFF',
-                    //align: 'left',
-                    verticalAlign: 'top',
-                    align: 'right',
-                    y: 60,
-                    x: -60
-                },
-                tooltip: {
-                    formatter: function () {
-                        return '<b>' + this.series.name + '</b><br/>' +
-                            capitalizeFirstLetter(this.x) + ': ' + this.y;
+
+                for (var i = 0; i < response.data.length; i++) {
+                    $scope.country.push(capitalizeFirstLetter($scope.data[i].province) + " " + $scope.data[i].year);
+                    $scope.year.push(($scope.data[i].year));
+                    $scope.priceaceite.push(Number($scope.data[i].priceaceite));
+                    $scope.pricevirgen.push(Number($scope.data[i].pricevirgen));
+                    $scope.priceextra.push(Number($scope.data[i].priceextra));
+                    $scope.gdp.push(null);
+                    $scope.gdp_growth.push(null);
+                    $scope.gdp_deflator.push(null);
+
+                    //console.log($scope.data[i].province);
+                    //console.log(JSON.stringify($scope.priceaceite, null, 2));
+
+                }
+                console.log(JSON.stringify($scope.country, null, 2));
+
+                $http.get("/api/v1/gdp/" + "?" + "apikey=" + $scope.apikey).then(function(response) {
+
+                    dataCache = response.data;
+                    $scope.data = dataCache;
+
+                    for (var i = 0; i < response.data.length; i++) {
+                        $scope.country.push(capitalizeFirstLetter($scope.data[i].country) + " " + $scope.data[i].year);
+                        //$scope.year.push(Number($scope.data[i].year));
+                        $scope.gdp.push(Number($scope.data[i].gdp));
+                        $scope.gdp_growth.push(Number($scope.data[i].gdp_growth));
+                        $scope.gdp_deflator.push(Number($scope.data[i].gdp_deflator));
+                        $scope.priceaceite.push(null);
+                        $scope.priceextra.push(null);
+                        $scope.pricevirgen.push(null);
+
+                        console.log($scope.data[i].country);
                     }
-                },
-                series:[{
-                    name: 'Year',
-                    data: $scope.year
-                }, {
-                    name: 'Gdp',
-                    data: $scope.gdp
-                }, {
-                    name: 'Gdp_Growth',
-                    data: $scope.gdp_growth
-                }, {
-                    name: 'Gdp_Deflator',
-                    data: $scope.gdp_deflator
-                }, {
-                    name: 'Integration with G04 Price of Olive oil in Andalussian',
-                    data: $scope.priceaceite
-                }]
-            
+                    Highcharts.chart('container', {
+                        title: {
+                            text: 'Highcharts'
+                        },
+                        chart: {
+                            type: 'area'
+                        },
+                        xAxis: {
+                            categories: $scope.country
+                        },
+                        legend: {
+                            layout: 'vertical',
+                            floating: true,
+                            backgroundColor: '#FFFFFF',
+                            //align: 'left',
+                            verticalAlign: 'top',
+                            align: 'right',
+                            y: 60,
+                            x: -60
+                        },
+                        tooltip: {
+                            formatter: function() {
+                                return '<b>' + this.series.name + '</b><br/>' +
+                                    capitalizeFirstLetter(this.x) + ': ' + this.y;
+                            }
+                        },
+                        series: [{
+                            name: 'Year',
+                            data: $scope.year
+                        }, {
+                            name: 'Gdp',
+                            data: $scope.gdp
+                        }, {
+                            name: 'Gdp_Growth',
+                            data: $scope.gdp_growth
+                        }, {
+                            name: 'Gdp_Deflator',
+                            data: $scope.gdp_deflator
+                        }, {
+                            name: 'Integration with G04 Price of Olive oil in Andalussian',
+                            data: $scope.priceaceite
+                        }]
 
 
+
+                    });
+
+
+
+                });
             });
 
-
-
-     });
-}]);
+    }]);
--- public/gdp/gdp-remote-graph-ctrl.js
+++ public/gdp/gdp-remote-graph-ctrl.js
@@ -2,11 +2,16 @@
 /* global angular */
 angular
     .module("DataManagementApp")
-    .controller("GdpRemoteGraphCtrl",["$scope","$http",function ($scope, $http){
+    .controller("GdpRemoteGraphCtrl",["$scope","$http", "$rootScope",function ($scope, $http ,$rootScope){
         
         $scope.apikey = "secret";
         $scope.data = {};
         var dataCache = {};
+        $scope.country = [];
+        $scope.year = [];
+        $scope.gdp = [];
+        $scope.gdp_growth = [];
+        $scope.gdp_deflator = [];
       
         
         $scope.minimumSalary= [];
@@ -17,24 +22,8 @@ angular
                 return string.charAt(0).toUpperCase() + string.slice(1);
             }
         
-        $http.get("/api/v1/gdp/"+ "?" + "apikey=" + $scope.apikey).then(function(response){
-            
-            dataCache = response.data;
-            $scope.data = dataCache;
-            
-            for(var i=0; i<response.data.length; i++){
-                $scope.country.push(capitalizeFirstLetter($scope.data[i].country) + " " + $scope.data[i].year);
-                $scope.year.push(Number($scope.data[i].year));
-                $scope.gdp.push(Number($scope.data[i].gdp));
-                $scope.gdp_growth.push(Number($scope.data[i].gdp_growth));
-                $scope.gdp_deflator.push(Number($scope.data[i].gdp_deflator));
-                // $scope.minimumSalary.push(null);
-                // $scope.averageSalary.push(null);
-                // $scope.riskOfPoverty.push(null);
-                
-                console.log($scope.data[i].country);
-            }
-        });
+        
+        
         
         $http.get("https://sos1617-07.herokuapp.com/api/v1/salaries/?apikey=sos07").then(function(response){
             
@@ -43,32 +32,33 @@ angular
             
             for(var i=0; i<response.data.length; i++){
                 
-                // if($scope.country.indexOf(capitalizeFirstLetter($scope.data[i].country) + " " + $scope.data[i].year)==-1){
-                //     // $scope.country.push(capitalizeFirstLetter($scope.data[i].country) + " " + $scope.data[i].year);
-                //     // $scope.year.push(null);
-                //     // $scope.gdp.push(null);
-                //     // $scope.gdp_growth.push(null);
-                //     // $scope.gdp_deflator.push(null);
+                
                     $scope.minimumSalary.push(Number($scope.data[i].minimumSalary));
                     $scope.averageSalary.push(Number($scope.data[i].averageSalary));
                     $scope.riskOfPoverty.push(Number($scope.data[i].riskOfPoverty));
                 
                     console.log($scope.data[i].country);
                     
-                // }else{
-                //     var index = $scope.country.indexOf(capitalizeFirstLetter(capitalizeFirstLetter($scope.data[i].country) + " " + $scope.data[i].year));
-                //     $scope.minimumSalary.push(index,1,Number($scope.data[i].minimumSalary));
-                //     $scope.averageSalary.push(index,1,Number($scope.data[i].averageSalary));
-                //     $scope.riskOfPoverty.push(index,1,Number($scope.data[i].riskOfPoverty));
-                // }
+                
             }
-        });
+            $http.get("/api/v1/gdp/"+ "?" + "apikey=" + $scope.apikey).then(function(response){
             
-        console.log("Controller initialized");
-        $http.get("https://sos1617-07.herokuapp.com/api/v1/salaries/?apikey=sos07").then(function(response){
+                dataCache = response.data;
+                $scope.data = dataCache;
             
+                for(var i=0; i<response.data.length; i++){
+                    $scope.country.push(capitalizeFirstLetter($scope.data[i].country) + " " + $scope.data[i].year);
+                    $scope.year.push(Number($scope.data[i].year));
+                    $scope.gdp.push(Number($scope.data[i].gdp));
+                    $scope.gdp_growth.push(Number($scope.data[i].gdp_growth));
+                    $scope.gdp_deflator.push(Number($scope.data[i].gdp_deflator));
             
-            Highcharts.chart('container', {
+                
+                
+            }console.log("Controller initialized (GdpRemoteGraphCtrl)");
+        
+    
+                Highcharts.chart('container', {
                 chart: {
                     type: 'spline'
                 },
@@ -140,4 +130,5 @@ angular
             });
             
         });
+            });
     }]);
