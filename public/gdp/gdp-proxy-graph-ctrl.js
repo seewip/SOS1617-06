923c81c1237ccbad8bdad53b62815e3ad98a4e01
SOS161707md <mateuszjacek95@gmail.com>
2017-05-16 21:13:06 +0000

    Debug code for gdp proxy

--- public/gdp/gdp-proxy-graph-ctrl.js
+++ public/gdp/gdp-proxy-graph-ctrl.js
@@ -27,6 +27,7 @@ angular
 
 
         function capitalizeFirstLetter(string) {
+            console.log("Captitalizing string: "+string);
             return string.charAt(0).toUpperCase() + string.slice(1);
         }
 
@@ -53,74 +54,74 @@ angular
 
                 }
                 console.log(JSON.stringify($scope.country, null, 2));
-                
-                        $http.get("/api/v1/gdp/" + "?" + "apikey=" + $scope.apikey).then(function(response) {
 
-            dataCache = response.data;
-            $scope.data = dataCache;
+                $http.get("/api/v1/gdp/" + "?" + "apikey=" + $scope.apikey).then(function(response) {
 
-            for (var i = 0; i < response.data.length; i++) {
-                $scope.country.push(capitalizeFirstLetter($scope.data[i].country) + " " + $scope.data[i].year);
-                //$scope.year.push(Number($scope.data[i].year));
-                $scope.gdp.push(Number($scope.data[i].gdp));
-                $scope.gdp_growth.push(Number($scope.data[i].gdp_growth));
-                $scope.gdp_deflator.push(Number($scope.data[i].gdp_deflator));
-                $scope.priceaceite.push(null);
-                $scope.priceextra.push(null);
-                $scope.pricevirgen.push(null);
+                    dataCache = response.data;
+                    $scope.data = dataCache;
 
-                console.log($scope.data[i].country);
-            }
-            Highcharts.chart('container', {
-                title: {
-                    text: 'Highcharts'
-                },
-                chart: {
-                    type: 'area'
-                },
-                xAxis: {
-                    categories: $scope.country
-                },
-                legend: {
-                    layout: 'vertical',
-                    floating: true,
-                    backgroundColor: '#FFFFFF',
-                    //align: 'left',
-                    verticalAlign: 'top',
-                    align: 'right',
-                    y: 60,
-                    x: -60
-                },
-                tooltip: {
-                    formatter: function() {
-                        return '<b>' + this.series.name + '</b><br/>' +
-                            capitalizeFirstLetter(this.x) + ': ' + this.y;
+                    for (var i = 0; i < response.data.length; i++) {
+                        $scope.country.push(capitalizeFirstLetter($scope.data[i].country) + " " + $scope.data[i].year);
+                        //$scope.year.push(Number($scope.data[i].year));
+                        $scope.gdp.push(Number($scope.data[i].gdp));
+                        $scope.gdp_growth.push(Number($scope.data[i].gdp_growth));
+                        $scope.gdp_deflator.push(Number($scope.data[i].gdp_deflator));
+                        $scope.priceaceite.push(null);
+                        $scope.priceextra.push(null);
+                        $scope.pricevirgen.push(null);
+
+                        console.log($scope.data[i].country);
                     }
-                },
-                series: [{
-                    name: 'Year',
-                    data: $scope.year
-                }, {
-                    name: 'Gdp',
-                    data: $scope.gdp
-                }, {
-                    name: 'Gdp_Growth',
-                    data: $scope.gdp_growth
-                }, {
-                    name: 'Gdp_Deflator',
-                    data: $scope.gdp_deflator
-                }, {
-                    name: 'Integration with G04 Price of Olive oil in Andalussian',
-                    data: $scope.priceaceite
-                }]
+                    Highcharts.chart('container', {
+                        title: {
+                            text: 'Highcharts'
+                        },
+                        chart: {
+                            type: 'area'
+                        },
+                        xAxis: {
+                            categories: $scope.country
+                        },
+                        legend: {
+                            layout: 'vertical',
+                            floating: true,
+                            backgroundColor: '#FFFFFF',
+                            //align: 'left',
+                            verticalAlign: 'top',
+                            align: 'right',
+                            y: 60,
+                            x: -60
+                        },
+                        tooltip: {
+                            formatter: function() {
+                                return '<b>' + this.series.name + '</b><br/>' +
+                                    capitalizeFirstLetter(this.x) + ': ' + this.y;
+                            }
+                        },
+                        series: [{
+                            name: 'Year',
+                            data: $scope.year
+                        }, {
+                            name: 'Gdp',
+                            data: $scope.gdp
+                        }, {
+                            name: 'Gdp_Growth',
+                            data: $scope.gdp_growth
+                        }, {
+                            name: 'Gdp_Deflator',
+                            data: $scope.gdp_deflator
+                        }, {
+                            name: 'Integration with G04 Price of Olive oil in Andalussian',
+                            data: $scope.priceaceite
+                        }]
 
 
 
-            });
+                    });
 
 
 
-        });
+                });
             });
 
     }]);
