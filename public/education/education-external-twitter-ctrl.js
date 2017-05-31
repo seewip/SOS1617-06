374603ead8c225aeb1ec3874223b396ab3d5c1e3
SOS161707md <mateuszjacek95@gmail.com>
2017-05-31 18:59:56 +0000

    Twitter changes

--- public/education/education-external-twitter-ctrl.js
+++ public/education/education-external-twitter-ctrl.js
@@ -10,7 +10,7 @@ controller("EducationExternalTwitterGraphCtrl", ["$scope", "$http", "$rootScope"
 
     if (!$rootScope.apikey) $rootScope.apikey = "secret";
 
-    $scope.refresh = function(key, secret) {
+    $scope.refresh = function(token) {
         $scope.loading = true;
         $http
             .get("../api/v1/education" + "?" + "apikey=" + $rootScope.apikey)
@@ -23,145 +23,133 @@ controller("EducationExternalTwitterGraphCtrl", ["$scope", "$http", "$rootScope"
                 var countriesData = [];
                 var provincesDataForeign = [];
 
-                // Get bearer token
                 $http
-                    .post("https://api.twitter.com/oauth2/token", "grant_type=client_credentials", {
+                    .get("https://api.twitter.com/1.1/friends/list.json?cursor=-1&screen_name=mafiu95&skip_status=true&include_user_entities=false", {
                         headers: {
-                            'Authorization': 'Basic '+btoa(key+":"+secret),
-                            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+                            'Authorization': 'bearer '+token
                         }
                     })
-                    .then(function(response_token) {
-                        console.log("TOKEN!! "+response_token );
-                        
-                        $http
-                            .get("#", {
-                                headers: {
-                                    
-                                }
-                            })
-                            .then(function(response_foreign) {
+                    .then(function(response_foreign) {
 
-                                console.log(response_foreign.data);
+                        console.log(response_foreign.data);
 
-                                response.data.forEach(function(d) {
-                                    if (years.indexOf(Number(d.year)) == -1) years.push(Number(d.year));
-                                    if (countries.indexOf(d.country) == -1) countries.push(d.country);
-                                });
+                        response.data.forEach(function(d) {
+                            if (years.indexOf(Number(d.year)) == -1) years.push(Number(d.year));
+                            if (countries.indexOf(d.country) == -1) countries.push(d.country);
+                        });
 
-                                response_foreign.data.data.forEach(function(d) {
-                                    if (years.indexOf(Number(d.fan_count)) == -1) years.push(Number(d.fan_count));
-                                    if (provincesForeign.indexOf(d.name) == -1) provincesForeign.push(d.name);
-                                });
+                        response_foreign.data.data.forEach(function(d) {
+                            if (years.indexOf(Number(d.fan_count)) == -1) years.push(Number(d.fan_count));
+                            if (provincesForeign.indexOf(d.name) == -1) provincesForeign.push(d.name);
+                        });
 
-                                years.sort((a, b) => a - b);
+                        years.sort((a, b) => a - b);
 
-                                countries.forEach(function(d) {
-                                    var b = {
-                                        name: d,
-                                        type: "column",
-                                        yAxis: 0,
-                                        data: []
-                                    };
-                                    years.forEach(function(e) {
-                                        b.data.push(0);
-                                    });
-                                    countriesData.push(b);
-                                });
-
-                                provincesForeign.forEach(function(d) {
-                                    var c = {
-                                        name: d,
-                                        type: "column",
-                                        yAxis: 1,
-                                        data: []
-                                    };
-                                    years.forEach(function(e) {
-                                        c.data.push(null);
-                                    });
-                                    provincesDataForeign.push(c);
-                                });
-
-                                response.data.forEach(function(d) {
-                                    countriesData.forEach(function(e) {
-                                        if (d.country === e.name) {
-                                            e.data[years.indexOf(Number(d.year))] = Number(d['education-gdp-perc']);
-                                        }
-                                    });
-                                });
-
-                                response_foreign.data.data.forEach(function(d) {
-                                    provincesDataForeign.forEach(function(e) {
-                                        if (d.name === e.name) {
-                                            e.data[years.indexOf(Number(d.fan_count))] = Number(d['fan_count']);
-                                        }
-                                    });
-                                });
-
-                                // $scope.debug += "#COUNTRIES:\n" + JSON.stringify(countries, null, 2);
-                                // $scope.debug += "#YEARS:\n" + JSON.stringify(years, null, 2);
-                                // $scope.debug += "#DATA:\n" + JSON.stringify(countriesData, null, 2);
-                                // $scope.debug += "#DATA_FOREIGN:\n" + JSON.stringify(countriesDataForeign, null, 2);
-
-                                var hc = {
-                                    chart: {
-                                        zoomType: 'xy'
-                                    },
-                                    title: {
-                                        text: 'Spending on education and the amount of oil'
-                                    },
-                                    xAxis: {
-                                        categories: [],
-                                        crosshair: true
-                                    },
-                                    yAxis: [{ // Primary yAxis
-                                        labels: {
-                                            format: '{value} %',
-                                            style: {
-                                                color: Highcharts.getOptions().colors[1]
-                                            }
-                                        },
-                                        title: {
-                                            text: 'GDP (%)',
-                                            style: {
-                                                color: Highcharts.getOptions().colors[1]
-                                            }
-                                        }
-
-                                    }, { // Secondary yAxis
-                                        gridLineWidth: 0,
-                                        title: {
-                                            text: 'Amount of oil',
-                                            style: {
-                                                color: Highcharts.getOptions().colors[0]
-                                            }
-                                        },
-                                        labels: {
-                                            format: '{value}',
-                                            style: {
-                                                color: Highcharts.getOptions().colors[0]
-                                            }
-                                        },
-                                        opposite: true
-                                    }],
-                                    tooltip: {
-                                        shared: true
-                                    },
-                                    plotOptions: {
-                                        column: {
-                                            stacking: 'normal'
-                                        }
-                                    },
-                                    series: []
-                                };
-
-                                hc.xAxis.categories = years;
-                                hc.series = countriesData.concat(provincesDataForeign);
-
-                                Highcharts.chart('hc_column', hc);
-
-                                $scope.loading = false;
+                        countries.forEach(function(d) {
+                            var b = {
+                                name: d,
+                                type: "column",
+                                yAxis: 0,
+                                data: []
+                            };
+                            years.forEach(function(e) {
+                                b.data.push(0);
                             });
+                            countriesData.push(b);
+                        });
+
+                        provincesForeign.forEach(function(d) {
+                            var c = {
+                                name: d,
+                                type: "column",
+                                yAxis: 1,
+                                data: []
+                            };
+                            years.forEach(function(e) {
+                                c.data.push(null);
+                            });
+                            provincesDataForeign.push(c);
+                        });
+
+                        response.data.forEach(function(d) {
+                            countriesData.forEach(function(e) {
+                                if (d.country === e.name) {
+                                    e.data[years.indexOf(Number(d.year))] = Number(d['education-gdp-perc']);
+                                }
+                            });
+                        });
+
+                        response_foreign.data.data.forEach(function(d) {
+                            provincesDataForeign.forEach(function(e) {
+                                if (d.name === e.name) {
+                                    e.data[years.indexOf(Number(d.fan_count))] = Number(d['fan_count']);
+                                }
+                            });
+                        });
+
+                        // $scope.debug += "#COUNTRIES:\n" + JSON.stringify(countries, null, 2);
+                        // $scope.debug += "#YEARS:\n" + JSON.stringify(years, null, 2);
+                        // $scope.debug += "#DATA:\n" + JSON.stringify(countriesData, null, 2);
+                        // $scope.debug += "#DATA_FOREIGN:\n" + JSON.stringify(countriesDataForeign, null, 2);
+
+                        var hc = {
+                            chart: {
+                                zoomType: 'xy'
+                            },
+                            title: {
+                                text: 'Spending on education and the amount of oil'
+                            },
+                            xAxis: {
+                                categories: [],
+                                crosshair: true
+                            },
+                            yAxis: [{ // Primary yAxis
+                                labels: {
+                                    format: '{value} %',
+                                    style: {
+                                        color: Highcharts.getOptions().colors[1]
+                                    }
+                                },
+                                title: {
+                                    text: 'GDP (%)',
+                                    style: {
+                                        color: Highcharts.getOptions().colors[1]
+                                    }
+                                }
+
+                            }, { // Secondary yAxis
+                                gridLineWidth: 0,
+                                title: {
+                                    text: 'Amount of oil',
+                                    style: {
+                                        color: Highcharts.getOptions().colors[0]
+                                    }
+                                },
+                                labels: {
+                                    format: '{value}',
+                                    style: {
+                                        color: Highcharts.getOptions().colors[0]
+                                    }
+                                },
+                                opposite: true
+                            }],
+                            tooltip: {
+                                shared: true
+                            },
+                            plotOptions: {
+                                column: {
+                                    stacking: 'normal'
+                                }
+                            },
+                            series: []
+                        };
+
+                        hc.xAxis.categories = years;
+                        hc.series = countriesData.concat(provincesDataForeign);
+
+                        Highcharts.chart('hc_column', hc);
+
+                        $scope.loading = false;
                     });
             }, function(response) {
                 $scope.loading = false;
